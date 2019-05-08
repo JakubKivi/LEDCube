4fa031d68c1b29e982336781d59246c547f1bbdc
JakubKivi <panmcleon@gmail.com>
2019-05-08 16:12:47 +0200

    Blink works.

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,14 @@
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(8, OUTPUT);
+
 }
 
 void loop() {
+digitalWrite(8, 1);
+delay(1000);
+digitalWrite(8, 0);
+delay(1000);
   // put your main code here, to run repeatedly:
 }
